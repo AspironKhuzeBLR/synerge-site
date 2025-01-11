01166db75adba1baddc53c187d9fa7ddd9e1e8aa
vishal gehlot <vishal.gehlot@protiviti.co.in>
2025-01-11 13:31:02 +0530

    init site

---
+++ js/forhref.js
@@ -0,0 +1,56 @@
+function HomeSynerge() {
+    location.href = "https://www.synergeworkspace.com/index.html";
+}
+
+function weSynerge() {
+    location.href = "https://www.synergeworkspace.com/we.html";
+}
+
+function CoworkingSpaceSynerge() {
+    location.href = "https://www.synergeworkspace.com/coworkingspace.html";
+}
+
+function Blogs() {
+    location.href = "https://www.synergeworkspace.com/blogs/index.html";
+}
+
+function    helloHsr() {
+    location.href = "https://www.synergeworkspace.com/hsr.html";
+}
+function    hellojay() {
+    location.href = "https://www.synergeworkspace.com/jayanagar.html";
+}
+function    helloHennur() {
+    location.href = "https://www.synergeworkspace.com/hennur.html";
+}
+
+function PrivateOfficeSynerge() {
+    location.href = "https://www.synergeworkspace.com/privateoffice.html";
+}
+function locations() {
+    location.href = "https://www.synergeworkspace.com/locations.html";
+}
+
+function conferencespaceSynerge() {
+    location.href = "https://www.synergeworkspace.com/conferencespace.html";
+}
+
+function FacilitiesSynerge() {
+    location.href = "https://www.synergeworkspace.com/facilities.html";
+}
+
+function WorkinNatureSynerge() {
+    location.href = "https://www.synergeworkspace.com/workinnature.html";
+}
+
+function CommunitySynerge() {
+    location.href = "https://www.synergeworkspace.com/community.html";
+}
+
+function testimonials() {
+    location.href = "https://www.synergeworkspace.com/testimonials.html";
+}
+
+function FranchiseSynerge() {
+    location.href = "https://www.synergeworkspace.com/franchise.html";
+}
